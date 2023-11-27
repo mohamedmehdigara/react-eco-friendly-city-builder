eab47d1d60ccda9d956de542e640c1fb8cec4ad4
mohamedmehdigara <garamehdi57@yahoo.com>
2023-11-27 14:05:41 +0100

    edited files

--- src/components/PollutionMeter.js
+++ src/components/PollutionMeter.js
@@ -1,14 +1,40 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { PollutionMeterContainer } from '../styles';
 
-const PollutionMeter = ({  }) => {
+const PollutionMeter = ({ pollutionLevel }) => {
+  const getPollutionStatus = () => {
+    if (pollutionLevel < 30) {
+      return 'Low';
+    } else if (pollutionLevel < 70) {
+      return 'Moderate';
+    } else {
+      return 'High';
+    }
+  };
+
+  const getProgressBarColor = () => {
+    if (pollutionLevel < 30) {
+      return 'green';
+    } else if (pollutionLevel < 70) {
+      return 'yellow';
+    } else {
+      return 'red';
+    }
+  };
+
   return (
     <PollutionMeterContainer>
       <h2>Pollution</h2>
-      <p>Level: </p>
-      {/* You can add visuals like a progress bar or color-coded indicators */}
+      <p>Level: {pollutionLevel}</p>
+      <div className="progress-bar" style={{ width: `${pollutionLevel}%`, backgroundColor: getProgressBarColor() }} />
+      <p>Status: {getPollutionStatus()}</p>
     </PollutionMeterContainer>
   );
+};
+
+PollutionMeter.propTypes = {
+  pollutionLevel: PropTypes.number.isRequired,
 };
 
 export default PollutionMeter;
