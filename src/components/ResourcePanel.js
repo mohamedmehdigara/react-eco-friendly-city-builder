1ee07ea7be35c35c0884e770abbbbca08134aebe
mohamedmehdigara <garamehdi57@yahoo.com>
2023-11-27 14:08:30 +0100

    edited files

--- src/components/ResourcePanel.js
+++ src/components/ResourcePanel.js
@@ -1,15 +1,30 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { ResourcePanelContainer } from '../styles';
 
 const ResourcePanel = ({ resources }) => {
   return (
     <ResourcePanelContainer>
       <h2>Resources</h2>
-      <p>Money: ${resources.money}</p>
-      <p>Energy: {resources.energy} kW</p>
+      <div className="resource-item">
+        <span className="resource-label">Money:</span>
+        <span className="resource-value">${resources.money}</span>
+      </div>
+      <div className="resource-item">
+        <span className="resource-label">Energy:</span>
+        <span className="resource-value">{resources.energy} kW</span>
+      </div>
       {/* Add more resource types as needed */}
     </ResourcePanelContainer>
   );
+};
+
+ResourcePanel.propTypes = {
+  resources: PropTypes.shape({
+    money: PropTypes.number.isRequired,
+    energy: PropTypes.number.isRequired,
+    // Add more resource types as needed
+  }).isRequired,
 };
 
 export default ResourcePanel;
