6bc8fb2a0da4e07b7a2e88517e5b997b36c2fca7
mohamedmehdigara <garamehdi57@yahoo.com>
2023-11-27 14:02:25 +0100

    edited files

--- src/components/EducationCenter.js
+++ src/components/EducationCenter.js
@@ -1,4 +1,3 @@
-// EducationCenter.js
 import React from 'react';
 import PropTypes from 'prop-types';
 import { EducationCenterContainer } from '../styles';
@@ -6,7 +5,13 @@ import { EducationCenterContainer } from '../styles';
 const EducationCenter = ({ onBuildEducationCenter }) => (
   <EducationCenterContainer>
     <h2>Education Center</h2>
-    <button onClick={onBuildEducationCenter}>Build Education Center</button>
+    <p>Build an Education Center to raise awareness and boost eco-friendliness!</p>
+    <button onClick={onBuildEducationCenter}>
+      <span role="img" aria-label="School">
+        🏫
+      </span>{' '}
+      Build Education Center
+    </button>
   </EducationCenterContainer>
 );
 
