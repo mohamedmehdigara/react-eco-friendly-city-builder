6f3f7996158318962024e8227bafe7f451485211
mohamedmehdigara <garamehdi57@yahoo.com>
2023-12-01 17:14:50 +0100

    edited files

--- src/components/EducationCenter.js
+++ src/components/EducationCenter.js
@@ -1,15 +1,20 @@
 // EducationCenter.js
-import React, {useState} from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import { EducationCenterContainer } from '../styles';
 
-const EducationCenter = ({ onBuildEducationCenter, onUpgrade, onResearch, courses, workshops, researchProjects }) => {
-  const [citizens, setCitizens] = useState([]);
-
+const EducationCenter = ({
+  onBuildEducationCenter,
+  onUpgrade,
+  onResearch,
+  courses,
+  workshops,
+  researchProjects,
+  citizens,
+  setCitizens,
+}) => {
   const handleAdoptPractice = (practice) => {
     // Implement logic to update citizen awareness and adopted practices
-    // For now, I assume you have a function setCitizens to update citizens
-    // Replace this with your actual state update logic
     setCitizens((prevCitizens) =>
       prevCitizens.map((citizen) => ({
         ...citizen,
@@ -40,32 +45,46 @@ const EducationCenter = ({ onBuildEducationCenter, onUpgrade, onResearch, course
       {/* Display available courses */}
       <h3>Available Courses</h3>
       <ul>
-        {courses && courses.map((course) => (
-          <li key={course.id}>
-            {course.name} - <button onClick={() => onUpgrade(course.id)}>Upgrade</button>
-          </li>
-        ))}
+        {courses &&
+          courses.map((course) => (
+            <li key={course.id}>
+              {course.name} - <button onClick={() => onUpgrade(course.id)}>Upgrade</button>
+            </li>
+          ))}
       </ul>
 
       {/* Display available workshops */}
       <h3>Available Workshops</h3>
       <ul>
-        {workshops && workshops.map((workshop) => (
-          <li key={workshop.id}>
-            {workshop.name} - <button onClick={() => onUpgrade(workshop.id)}>Upgrade</button>
-          </li>
-        ))}
+        {workshops &&
+          workshops.map((workshop) => (
+            <li key={workshop.id}>
+              {workshop.name} - <button onClick={() => onUpgrade(workshop.id)}>Upgrade</button>
+            </li>
+          ))}
       </ul>
 
       {/* Display available research projects */}
       <h3>Research Projects</h3>
       <ul>
-        {researchProjects && researchProjects.map((project) => (
-          <li key={project.id}>
-            {project.name} - <button onClick={() => onResearch(project.id)}>Research</button>
-          </li>
-        ))}
+        {researchProjects &&
+          researchProjects.map((project) => (
+            <li key={project.id}>
+              {project.name} - <button onClick={() => onResearch(project.id)}>Research</button>
+            </li>
+          ))}
       </ul>
+
+      {/* Display citizen awareness and adoption */}
+      <h3>Citizen Awareness</h3>
+      {citizens &&
+        citizens.map((citizen) => (
+          <div key={citizen.id}>
+            <p>Citizen {citizen.id}</p>
+            <p>Awareness Level: {citizen.awarenessLevel}</p>
+            <p>Adopted Practices: {citizen.adoptedPractices.join(', ')}</p>
+          </div>
+        ))}
     </EducationCenterContainer>
   );
 };
@@ -77,6 +96,8 @@ EducationCenter.propTypes = {
   courses: PropTypes.arrayOf(PropTypes.object).isRequired,
   workshops: PropTypes.arrayOf(PropTypes.object).isRequired,
   researchProjects: PropTypes.arrayOf(PropTypes.object).isRequired,
+  citizens: PropTypes.array.isRequired,
+  setCitizens: PropTypes.func.isRequired,
 };
 
 export default EducationCenter;
