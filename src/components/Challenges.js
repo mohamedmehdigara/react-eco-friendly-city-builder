0898a695755d13f0f993baf1206c10c9044c67b1
mohamedmehdigara <garamehdi57@yahoo.com>
2023-12-03 14:52:21 +0100

    edited files

--- src/components/Challenges.js
+++ src/components/Challenges.js
@@ -1,11 +1,24 @@
 // Challenges.js
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 
-const Challenges = ({ challenges, onToggleChallenge }) => {
+const Challenges = ({ challenges, onToggleChallenge, onDeleteChallenge, onAddChallenge }) => {
+  const [newChallenge, setNewChallenge] = useState('');
+
   const handleToggleChallenge = (id) => {
     // Implement logic to toggle the completion status of the challenge
     onToggleChallenge(id);
+  };
+
+  const handleDeleteChallenge = (id) => {
+    // Implement logic to delete the challenge
+    onDeleteChallenge(id);
+  };
+
+  const handleAddChallenge = () => {
+    // Implement logic to add a new challenge
+    onAddChallenge(newChallenge);
+    setNewChallenge(''); // Clear the input field after adding a challenge
   };
 
   return (
@@ -14,13 +27,27 @@ const Challenges = ({ challenges, onToggleChallenge }) => {
       <ul>
         {challenges.map(({ id, description, completed }) => (
           <li key={id} style={getListItemStyle(completed)}>
-            {description}
-            <button onClick={() => handleToggleChallenge(id)}>
-              {completed ? 'Undo' : 'Complete'}
-            </button>
+            <span>{description}</span>
+            <div>
+              <button onClick={() => handleToggleChallenge(id)}>
+                {completed ? 'Undo' : 'Complete'}
+              </button>
+              <button onClick={() => handleDeleteChallenge(id)}>Delete</button>
+            </div>
           </li>
         ))}
       </ul>
+
+      {/* Form to add a new challenge */}
+      <div>
+        <input
+          type="text"
+          value={newChallenge}
+          onChange={(e) => setNewChallenge(e.target.value)}
+          placeholder="Enter a new challenge"
+        />
+        <button onClick={handleAddChallenge}>Add Challenge</button>
+      </div>
     </div>
   );
 };
@@ -34,6 +61,8 @@ Challenges.propTypes = {
     })
   ).isRequired,
   onToggleChallenge: PropTypes.func.isRequired,
+  onDeleteChallenge: PropTypes.func.isRequired,
+  onAddChallenge: PropTypes.func.isRequired,
 };
 
 const getListItemStyle = (completed) => ({
